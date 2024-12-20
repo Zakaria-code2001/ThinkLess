de89193e4c67672e0626a0e5ecd0b01216c8ad1d
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-12-20 20:31:51 +0100

    swagger ui for note service

--- backend/notes-service/database.py
+++ backend/notes-service/database.py
@@ -6,6 +6,6 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://admin:adminpass@db:5432/n
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
+
 def init_db():
     Base.metadata.create_all(bind=engine)
-
--- backend/notes-service/models/note.py
+++ backend/notes-service/models/note.py
@@ -2,17 +2,20 @@ from sqlalchemy import Column, Integer, String, Text, DateTime
 from datetime import datetime
 from database import Base, engine
 
+
 class Note(Base):
-    __tablename__ = 'notes'
-    
+    __tablename__ = "notes"
+
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
+
 def init_db():
     Base.metadata.create_all(engine)
+
 
 if __name__ == "__main__":
     init_db()
